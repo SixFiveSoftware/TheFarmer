b165a92dea59b3a6e5220ef6e1d2278354a9ccb4
BJ Miller <john.miller@dxydoes.com>
2015-06-27 20:16:10 -0400

    Add Farmer

---
+++ Farmer.swift
@@ -0,0 +1,11 @@
+//
+//  Farmer.swift
+//  TheFarmer
+//
+//  Created by BJ on 6/27/15.
+//  Copyright © 2015 Six Five Software, LLC. All rights reserved.
+//
+
+struct Farmer {
+    var beanCount = 1
+}
